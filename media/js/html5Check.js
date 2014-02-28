ff20b9189242c1ebf4305710ae5514e6b26148a2
hewen <HeWen.coder@gmail.com>
2014-02-28 23:30:06 +0800

    modify check html5

--- media/js/html5Check.js
+++ media/js/html5Check.js
@@ -1,6 +1,12 @@
-function checkhHtml5() {    
+/*function checkhHtml5() {    
     if (typeof(Worker) == "undefined") {  
         document.getElementById("html5Check").style.display = "block";
     }
-} 
+}
 checkhHtml5();
+*/
+$(document).ready(function(){
+    if (typeof(Worker) == "undefined") {  
+      $("html5Check").hide();
+    }
+});
